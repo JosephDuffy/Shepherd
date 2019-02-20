29942976c9e2ad86db0b7ffe59382d4ef181be54
Joseph Duffy <joseph.duffy@thread.com>
2019-02-20 21:46:51 +0000

    Initial Commit

---
+++ Source/Shepherd.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for Shepherd.
+FOUNDATION_EXPORT double ShepherdVersionNumber;
+
+//! Project version string for Shepherd.
+FOUNDATION_EXPORT const unsigned char ShepherdVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shepherd/PublicHeader.h>
