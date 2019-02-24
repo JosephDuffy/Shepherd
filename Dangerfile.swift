3edc076daa6b819ed7a50aa77599d3effa8dd8c5
Joseph Duffy <josephduffy@me.com>
2019-02-24 17:21:20 +0000

    Add commend to Dangerfile

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -48,6 +48,9 @@ struct VersionFile {
 
 let projectName = "Shepherd"
 let danger = Danger()
+// Danger only run for PRs, so if `github` isn't `nil` then
+// this is a PR on GitHub. This bool is used to check for
+// `swift run danger-swift ci` vs `swift run danger-swift local`
 let isPR = danger.github != nil
 
 func checkSwiftVersions() {
