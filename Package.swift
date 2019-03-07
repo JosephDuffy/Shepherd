f16fdad892b2232dd304da27540a503f4dfc26e3
Joseph Duffy <josephduffy@me.com>
2019-03-07 21:54:48 +0000

    Update to use my fork of danger-swift
    This branch fixed the SwiftLint plugin when using fish

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "DangerDeps", type: .dynamic, targets: ["DangerDeps"]), // dev
     ],
     dependencies: [
-        .package(url: "https://github.com/danger/swift.git", from: "1.0.0"), // dev
+        .package(url: "https://github.com/JosephDuffy/danger-swift.git", from: "1.0.0"), // dev
         .package(url: "https://github.com/Realm/SwiftLint", from: "0.30.1"), // dev
         .package(url: "https://github.com/f-meloni/Rocket", from: "0.1.0"), // dev
     ],
