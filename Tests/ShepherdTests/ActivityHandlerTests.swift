f235af74c2d2fe8e0f7de76243b36e2050af9d1c
Joseph Duffy <commits@josephduffy.co.uk>
2019-07-27 15:07:19 +0100

    Add tests for `ActivityHandler`

---
+++ Tests/ShepherdTests/ActivityHandlerTests.swift
@@ -0,0 +1,84 @@
+import Quick
+import Nimble
+@testable import Shepherd
+
+final class ActivityHandlerTests: QuickSpec {
+
+    override func spec() {
+        describe("ActivityHandler") {
+            var activityHandler: ActivityHandler!
+
+            beforeEach {
+                activityHandler = ActivityHandler()
+            }
+
+            context("with a parent") {
+                var parent: MockActivityHandlerAggregate!
+
+                beforeEach {
+                    parent = MockActivityHandlerAggregate()
+                    activityHandler.parent = parent
+                }
+
+                context("handle(activity:ignoring:)") {
+                    var activity: NSUserActivity!
+
+                    beforeEach {
+                        activity = NSUserActivity(activityType: "test-activity")
+                    }
+
+                    context("passing `nil` to `ignoring`") {
+                        var result: ActivityHandler?
+
+                        beforeEach {
+                            result = activityHandler.handle(activity: activity, ignoring: nil)
+                        }
+
+                        it("should call `handle(activity:ignoring:) on the parent") {
+                            expect(parent.latestHandleActivityParameters).toNot(beNil())
+                        }
+
+                        it("should pass the activity to the parent") {
+                            expect(parent.latestHandleActivityParameters?.activity) == activity
+                        }
+
+                        it("should pass itself to the parent") {
+                            expect(parent.latestHandleActivityParameters?.ignoring) === activityHandler
+                        }
+
+                        it("should return `nil`") {
+                            expect(result).to(beNil())
+                        }
+                    }
+
+                    context("passing the parent to `ignoring`") {
+                        var result: ActivityHandler?
+
+                        beforeEach {
+                            result = activityHandler.handle(activity: activity, ignoring: parent)
+                        }
+
+                        it("should not call `handle(activity:ignoring:) on the parent") {
+                            expect(parent.latestHandleActivityParameters).to(beNil())
+                        }
+
+                        it("should return `nil`") {
+                            expect(result).to(beNil())
+                        }
+                    }
+                }
+
+                context("when all other references to the parent has been nilled") {
+                    beforeEach {
+                        parent = nil
+                    }
+
+                    it("should not have a parent") {
+                        expect(activityHandler.parent).to(beNil())
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ Tests/ShepherdTests/Mocks/MockActivityHandlerAggregate.swift
@@ -0,0 +1,13 @@
+import Shepherd
+
+class MockActivityHandlerAggregate: ActivityHandlerAggregate {
+
+    private(set) var latestHandleActivityParameters: (activity: NSUserActivity, ignoring: ActivityHandler?)?
+
+    override func handle(activity: NSUserActivity, ignoring: ActivityHandler? = nil) -> ActivityHandler? {
+        latestHandleActivityParameters = (activity, ignoring)
+
+        return super.handle(activity: activity, ignoring: ignoring)
+    }
+
+}
