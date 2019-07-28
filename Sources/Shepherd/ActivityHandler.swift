89e999d42ee11803544f1ccae53da4bc2f2604be
Joseph Duffy <commits@josephduffy.co.uk>
2019-07-28 01:01:32 +0100

    Update documentation

--- Sources/Shepherd/ActivityHandler.swift
+++ Sources/Shepherd/ActivityHandler.swift
@@ -1,19 +1,29 @@
 import Foundation
 
+/**
+ An object that can handle zero or more kinds of `NSUserActivity`s.
+
+ By default this class with not handle any activities itself.
+ */
 open class ActivityHandler {
 
+    /// The immediate parent. This will be set automatically by the parent.
     open weak var parent: ActivityHandlerAggregate?
 
+    /// Create an activity handler.
     public init() {}
 
     /**
-     Attempts to handle the provided activity. Subclasses should override this function and handle
-     any activities that can be handled, in which case `self` should be returned. If the activity should
-     not be handled further return `nil`, otherwise return the result of a call to `super`.
+     Attempts to handle the provided activity. If the activity cannot be handled it will be ask the parent to handle
+     the activity, unless the `ignoring` parameter is set to parent.
 
-     - parameter activity: The activity to be handled
-     - parameter ignoring: An activity handler to ignore when traversing the handlers tree
-     - returns: The activity handler that handled the activity
+     Subclasses should override this function and handle any activities that can be handled, in which case `self` should
+     be returned. If the activity should not be handled further return `nil`, otherwise return the result of a call to
+     `super`.
+
+     - parameter activity: The activity to be handled.
+     - parameter ignoring: An optional activity handler to ignore. The only valid values are the parent and `nil`.
+     - returns: The activity handler that handled the activity, or `nil` if it was not handled.
      */
     @discardableResult
     open func handle(activity: NSUserActivity, ignoring: ActivityHandler? = nil) -> ActivityHandler? {
--- Sources/Shepherd/ActivityHandlerAggregate.swift
+++ Sources/Shepherd/ActivityHandlerAggregate.swift
@@ -1,18 +1,27 @@
 import Foundation
 
+/**
+ An activity handler that can have child activity handlers. When handling an activity it will ask the children to handle
+ the activity before asking the parent to handle it.
+*/
 open class ActivityHandlerAggregate: ActivityHandler {
 
     private var _children: [ObjectStorage<ActivityHandler>] = []
 
+    /// The children that will be asked to handle activities.
     public var children: [ActivityHandler] {
         return _children.compactMap { $0.object }
     }
 
+    /// Create an activity handler aggregate.
     public override init() {}
 
     /**
      Adds the activity handler to the array of children that will be queried when attempting to handle
-     an activity. It will also set the parent to this aggregate.
+     an activity. The `parent` of `child` will be set to this aggregate.
+
+     - Parameter child: The child to add.
+     - Parameter storageOption: How to store the object, weakly or strongly.
      */
     open func append(_ child: ActivityHandler, heldOnTo storageOption: StorageOption) {
         _children.append(storageOption.store(child))
@@ -21,7 +30,10 @@ open class ActivityHandlerAggregate: ActivityHandler {
 
     /**
      Removes the activity handler from the array of children that will be queried when attempting to handle
-     an activity. It will also set the parent to `nil`
+     an activity. If the passed activity handler was a child of this activity handler the `parent` property will be set
+     to `nil`.
+
+     - Parameter child: The child to be removed.
      */
     open func remove(_ child: ActivityHandler) {
         let countBefore = _children.count
@@ -35,11 +47,12 @@ open class ActivityHandlerAggregate: ActivityHandler {
 
     /**
      Attempts to handle the provided activity by asking children to handle the activity. If none
-     of the children can handle the activity the parent will also be queried.
+     of the children handle the activity the parent will also be asked to handle it.
 
-     - Parameter activity: The activity to be handled
-     - Parameter ignoring: An optional activity handler that should be ignored while traversing
-     - Returns: The handler that handled the activity, or `nil` if the activity was not handled
+     - Parameter activity: The activity to be handled.
+     - Parameter ignoring: An optional activity handler that should be ignored. The only valid value is one of the
+                           children, the parent, or `nil`.
+     - Returns: The handler that handled the activity, or `nil` if the activity was not handled.
      */
     @discardableResult
     open override func handle(activity: NSUserActivity, ignoring: ActivityHandler? = nil) -> ActivityHandler? {
