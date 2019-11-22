6835f14ed2b61812377db35477b7c6ba1e3ad9be
Joseph Duffy <commits@josephduffy.co.uk>
2019-11-22 17:33:10 +0000

    Update Priority to be a struct

--- Sources/Shepherd/Priority.swift
+++ Sources/Shepherd/Priority.swift
@@ -1,20 +1,30 @@
-public enum Priority: Int, Hashable, Comparable, RawRepresentable {
 /// The priority of a path handler within a router. Path handlers are queried in decending priority order.
+public struct Priority: RawRepresentable, ExpressibleByIntegerLiteral, Hashable, Comparable {
 
     public static func < (lhs: Priority, rhs: Priority) -> Bool {
         return lhs.rawValue < rhs.rawValue
     }
 
     /// A high priority. Equal to 1000.
-    case high = 1000
+    public static let high: Priority = 1000
 
     /// A medium priority. Equal to 500.
-    case medium = 500
+    public static let medium: Priority = 500
 
     /// A low priority. Equal to 100.
-    case low = 100
+    public static let low: Priority = 100
 
     /// The priority of a parent router. Equal to 0.
-    case parent = 0
+    public static let parent: Priority = 0
+
+    public let rawValue: Int
+
+    public init(rawValue: Int) {
+        self.rawValue = rawValue
+    }
+
+    public init(integerLiteral value: Int) {
+        rawValue = value
+    }
 
 }
