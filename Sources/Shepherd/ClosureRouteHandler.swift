fc3d1ba60e09d8ca8570ae3430af368c6d94ea63
Joseph Duffy <commits@josephduffy.co.uk>
2019-11-22 17:39:37 +0000

    Add documentation to ClosurePathHandler

--- Sources/Shepherd/ClosureRouteHandler.swift
+++ Sources/Shepherd/ClosureRouteHandler.swift
@@ -23,14 +23,30 @@ internal final class ClosurePathHandler<Path>: PathHandler {
 
 extension Router {
 
+    /**
+     A closure that can be queried when attempting to handle a path.
+
+     - Parameter path: The path to attempt to handle.
+     - Parameter completionHandler: A closure to be called when the path has been handled, or it is determined the path
+                                    cannot be handled.
+     */
     public typealias ClosurePathHandler<Path> = (_ path: Path, _ completionHandler: @escaping (_ didHandle: Bool) -> Void) -> Void
 
+    /**
+     Add the provided closure to be a path handler.
+
+     When the closure is called it should attempt to handle the path. If the path is handled the closure should be
+     called with `true`, otherwise the closure should be called with `false`.
+
+     - Parameter pathHandler: A closure that will be queried when handling a path of type `Path`
+     - Parameter priority: The priority of the handler.
+     */
     @discardableResult
-    public func add<Route>(
-        routeHandler: @escaping ClosurePathHandler<Route>,
+    public func add<Path>(
+        pathHandler: @escaping ClosurePathHandler<Path>,
         priority: Priority = .medium
     ) -> PathHandler {
-        let handler = Shepherd.ClosurePathHandler(handler: routeHandler)
+        let handler = Shepherd.ClosurePathHandler(handler: pathHandler)
         add(child: handler, priority: priority)
         return handler
     }
