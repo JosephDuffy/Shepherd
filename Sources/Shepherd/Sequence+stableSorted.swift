bd759b34c8bdc4ba6acb2ac2e637afce58ca4422
Joseph Duffy <commits@josephduffy.co.uk>
2019-11-25 13:03:47 -0800

    Disable identifier_name in stableSorted

--- Sources/Shepherd/Sequence+stableSorted.swift
+++ Sources/Shepherd/Sequence+stableSorted.swift
@@ -1,5 +1,6 @@
 extension Sequence {
     internal func stableSorted(by areInIncreasingOrder: (Element, Element) throws -> Bool) rethrows -> [Element] {
+        // swiftlint:disable:identifier_name
         return try enumerated().sorted { a, b in
             try areInIncreasingOrder(a.element, b.element) ||
                 (a.offset < b.offset && !areInIncreasingOrder(b.element, a.element))
