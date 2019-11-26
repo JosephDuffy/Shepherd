031680517558a1274ad269facb675a931f29ec71
Joseph Duffy <commits@josephduffy.co.uk>
2019-11-26 08:28:07 -0800

    Rename XCRouterTests to RouterTests

--- Tests/ShepherdTests/RouterTests.swift
+++ Tests/ShepherdTests/RouterTests.swift
@@ -1,7 +1,7 @@
 import XCTest
 @testable import Shepherd
 
-final class XCRouterTests: XCTestCase {
+final class RouterTests: XCTestCase {
 
     func testEmptyRouter() {
         let router = Router()
